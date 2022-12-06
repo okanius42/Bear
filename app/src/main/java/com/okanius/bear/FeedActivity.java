1a9f3789231061527530e156e58b02fc7c56f9c1
okanius42 <okanalgil@gmail.com>
2022-12-07 01:03:29 +0300

    okanius
    Music note images added.
    UploadActivity and FeedActivity upgraded.

--- app/src/main/java/com/okanius/bear/FeedActivity.java
+++ app/src/main/java/com/okanius/bear/FeedActivity.java
@@ -1,15 +1,30 @@
 package com.okanius.bear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.okanius.bear.databinding.ActivityFeedBinding;
+import com.squareup.picasso.Picasso;
 
 public class FeedActivity extends AppCompatActivity {
     private ActivityFeedBinding binding;
+    FirebaseUser firebaseUser;
+    FirebaseStorage firebaseStorage;
+    FirebaseFirestore firebaseFirestore;
+    DocumentReference documentReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,29 @@ public class FeedActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(FeedActivity.this,ProfileActivity.class));
+            }
+        });
+        binding.uploadF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FeedActivity.this,UploadActivity.class));
+            }
+        });
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        String email = firebaseUser.getEmail();
+        documentReference = firebaseFirestore.collection("users").document(email);
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()){
+                        binding.somewords.setText((String) "nick : " + documentSnapshot.getData().get("username"));
+                        Picasso.get().load((String) documentSnapshot.getData().get("profilePhotoUri")).into(binding.bearF);
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/okanius/bear/UploadActivity.java
+++ app/src/main/java/com/okanius/bear/UploadActivity.java
@@ -15,9 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.okanius.bear.databinding.ActivityUploadBinding;
+import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
 import java.util.HashMap;
@@ -25,9 +27,9 @@ import java.util.HashMap;
 public class UploadActivity extends AppCompatActivity {
     private ActivityUploadBinding binding;
     FirebaseUser firebaseUser;
-    FirebaseFirestore firebaseFirestore;
     FirebaseStorage firebaseStorage;
-
+    FirebaseFirestore firebaseFirestore;
+    DocumentReference documentReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,17 +37,37 @@ public class UploadActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         firebaseStorage = FirebaseStorage.getInstance();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        String email = firebaseUser.getEmail();
+        documentReference = firebaseFirestore.collection("users").document(email);
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if(documentSnapshot.exists()){
+                        binding.usernameU.setText((String) "nick : " + documentSnapshot.getData().get("username"));
+                        Picasso.get().load((String) documentSnapshot.getData().get("profilePhotoUri")).into(binding.bearU);
+                    }
+                }
+            }
+        });
     }
 
-    DocumentReference documentReference;
+    public class arrays {
+
+    }
+
+
     Calendar calendar;
     int day,month,year,hour,minute,second;
-    Uri photoUri;
+
     public void share(View view){
         String email = firebaseUser.getEmail();
-        String comment = binding.commentU.getText().toString();
         HashMap<String,Object> post = new HashMap<>();
-        post.put("comment",comment);
 
         calendar = Calendar.getInstance();
         day = calendar.get(Calendar.DAY_OF_MONTH); post.put("day",day);
